0228ce6afe56bf451d2d9535fed83426dc0fbb77
Kang-xiao-zhuang <2247830091@qq.com>
2023-04-01 22:49:34 +0800

    Mapper层

---
+++ src/main/java/com/zhuang/docker/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.zhuang.docker.mapper;
+
+import com.zhuang.docker.entity.User;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * description: UserMapper
+ * date: 2023/3/31 13:24
+ * author: Zhuang
+ * version: 1.0
+ */
+public interface UserMapper extends Mapper<User> {
+}
